bf5620668a395c7c61fc6a5b72b906fb599e666e
Jatin Sharma <jatinkumar2040@gmail.com>
2025-05-13 15:05:53 +0530

    web hooks added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 
 //App config
@@ -16,6 +17,7 @@ app.use(cors())
 
 //API routes
 app.get('/', (req, res) => res.send('API Working'))
+app.use('/api/user', userRouter);
 
 app.listen(PORT, ()=> console.log('Server running on port '+PORT)
 )
